a977fb8e211ce70a08bfbd7ff7553246c0f7d353
Gavin Nelson <gavin@nelson.co>
2021-04-02 17:41:21 -0400

    test commit

--- Landmarks/ContentView.swift
+++ Landmarks/ContentView.swift
@@ -13,6 +13,7 @@ struct ContentView: View {
             Text("Turtle Rock")
                 .font(.title)
                 .foregroundColor(.black)
+                .padding()
         }
             
     }
