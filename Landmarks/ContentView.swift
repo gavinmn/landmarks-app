4e78b9494fae2ca1438fce69e2976108b776975a
Gavin Nelson <gavin@nelson.co>
2021-04-02 17:40:50 -0400

    initial commit

--- Landmarks/ContentView.swift
+++ Landmarks/ContentView.swift
@@ -9,8 +9,12 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Text("Turtle Rock")
+                .font(.title)
+                .foregroundColor(.black)
+        }
+            
     }
 }
 
