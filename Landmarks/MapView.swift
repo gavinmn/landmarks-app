bdbf3af9823c2a24d4193d5752e944e1b7c32058
Gavin Nelson <gavin@nelson.co>
2021-04-02 20:10:32 -0400

    finished circle image view

---
+++ Landmarks/MapView.swift
@@ -0,0 +1,20 @@
+//
+//  MapView.swift
+//  Landmarks
+//
+//  Created by Gavin Nelson on 4/2/21.
+//
+
+import SwiftUI
+
+struct MapView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MapView_Previews: PreviewProvider {
+    static var previews: some View {
+        MapView()
+    }
+}
