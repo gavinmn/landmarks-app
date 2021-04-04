a413102e260379084a022f29a1b50d5fa54418ec
Gavin Nelson <gavin@nelson.co>
2021-04-03 20:19:51 -0400

    user input section 3 done

--- Landmarks/Views/LandmarkList.swift
+++ Landmarks/Views/LandmarkList.swift
@@ -8,24 +8,39 @@
 import SwiftUI
 
 struct LandmarkList: View {
-    var body: some View {
-        NavigationView {
-            List(landmarks) { landmark in
-                NavigationLink(destination: LandmarkDetail(landmark: landmark)) {
-                    LandmarkRow(landmark: landmark)
-                }
+    
+    @State private var showFavoritesOnly = false
+    
+    var filteredLandmarks: [Landmark] {
+            landmarks.filter { landmark in
+                (!showFavoritesOnly || landmark.isFavorite)
             }
-            .navigationTitle("Landmarks")
+        }
+    
+    var body: some View {
+        
+        NavigationView {
+            
+            List {
+                Toggle(isOn: $showFavoritesOnly) {
+                    Text("Show Favorites Only")
+                }
+                ForEach(filteredLandmarks) { landmark in
+                    NavigationLink(destination: LandmarkDetail(landmark: landmark)) {
+                        LandmarkRow(landmark: landmark)
+                    }
+                }
+                .navigationTitle("Landmarks")
+                
+            }
+            
         }
     }
 }
 
 struct LandmarkList_Previews: PreviewProvider {
     static var previews: some View {
-        ForEach(["iPhone SE (2nd generation)", "iPhone XS Max"], id: \.self) { deviceName in
-            LandmarkList()
-                .previewDevice(PreviewDevice(rawValue: deviceName))
-                .previewDisplayName(deviceName)z
-        }
+        LandmarkList()
     }
 }
+
